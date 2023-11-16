ab7f0401c0010528d061e89a10c1dafc4046bbec
Alejandro <alejandro.lago@hotmail.com>
2023-11-16 23:55:46 +0100

    Creada copia de PruebaTemporizador2

---
+++ poo/PruebaTemporizador3.java
@@ -0,0 +1,57 @@
+package poo;
+
+import javax.swing.Timer;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+import java.awt.Toolkit;
+
+public class PruebaTemporizador3 {
+    
+    public static void main(String[] args) {
+
+        Reloj mi_reloj = new Reloj(3000, true);
+
+        mi_reloj.enMarcha();
+
+        JOptionPane.showMessageDialog(null, "Pulsa Aceptar para terminar.");
+
+        System.exit(0);
+        
+    }
+}
+
+class Reloj {
+
+    private int intervalo;
+    private boolean sonido;
+
+    public Reloj(int intervalo, boolean sonido) {
+
+        this.intervalo = intervalo;
+        this.sonido = sonido;
+
+    }
+
+    public void enMarcha() {
+
+        ActionListener oyente = new DameLaHora2();
+        Timer mi_temporizador = new Timer(intervalo, oyente);
+
+        mi_temporizador.start();
+    }
+
+    private class DameLaHora2 implements ActionListener {   // Las clases internas son útiles para entrar al código encapsulado a la que quieres que solo él entre
+
+        public void actionPerformed(ActionEvent evento) {
+
+            Date ahora = new Date();
+
+            System.out.println("Te pongo la hora cada 3 segundos | " + ahora);
+
+            if(sonido) {
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
+}
