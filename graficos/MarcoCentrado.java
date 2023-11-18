38aa6abe956cf918abb05ff4c1c86090fa2ea9d5
Alejandro <alejandro.lago@hotmail.com>
2023-11-18 01:00:09 +0100

    Recoger dimensiones de la pantalla

--- graficos/MarcoCentrado.java
+++ graficos/MarcoCentrado.java
@@ -1,5 +1,6 @@
 package graficos;
 
+import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.*;
 
@@ -17,7 +18,7 @@ class CreandoMarcoCentrado extends JFrame {
 public CreandoMarcoCentrado() {
 
     Toolkit mi_pantalla = Toolkit.getDefaultToolkit();
-
+    Dimension tamano_pantalla = mi_pantalla.getScreenSize();
 }
 
 }
