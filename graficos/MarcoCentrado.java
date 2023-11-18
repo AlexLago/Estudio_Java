c276a3a1599215bbc59e316990e4dce946fb0858
Alejandro <alejandro.lago@hotmail.com>
2023-11-18 01:08:26 +0100

    Añadido setTitle

--- graficos/MarcoCentrado.java
+++ graficos/MarcoCentrado.java
@@ -18,17 +18,18 @@ public class MarcoCentrado {
 
 class CreandoMarcoCentrado extends JFrame {
 
-public CreandoMarcoCentrado() {
+    public CreandoMarcoCentrado() {
 
-    Toolkit mi_pantalla = Toolkit.getDefaultToolkit();
-    Dimension tamano_pantalla = mi_pantalla.getScreenSize();
+        Toolkit mi_pantalla = Toolkit.getDefaultToolkit();
+        Dimension tamano_pantalla = mi_pantalla.getScreenSize();
 
-    int altura_pantalla = tamano_pantalla.height;
-    int ancho_pantalla = tamano_pantalla.width;
+        int altura_pantalla = tamano_pantalla.height;
+        int ancho_pantalla = tamano_pantalla.width;
 
-    setSize(ancho_pantalla/2, altura_pantalla/2);
-    setLocation(ancho_pantalla/4 , altura_pantalla/4);
+        setSize(ancho_pantalla/2, altura_pantalla/2);
+        setLocation(ancho_pantalla/4 , altura_pantalla/4);
+        setTitle("MarcoCentrado");
 
-}
+    }
 
 }
