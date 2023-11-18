b419b7402e474a8298aa0320fe8e49cd9666c1cb
Alejandro <alejandro.lago@hotmail.com>
2023-11-18 23:04:45 +0100

    Creada instancia MarcoConDibujos

--- graficos/PruebaDibujo.java
+++ graficos/PruebaDibujo.java
@@ -7,7 +7,7 @@ public class PruebaDibujo {
     
     public static void main(String[] args) {
         
-
+        MarcoConDibujos mi_marco = new MarcoConDibujos();
 
     }
 
@@ -17,7 +17,8 @@ class MarcoConDibujos extends JFrame {
 
     public MarcoConDibujos() {
 
-        
+        setTitle("Prueba de Dibujo");
+        setSize(400, 400);
 
     }
 
