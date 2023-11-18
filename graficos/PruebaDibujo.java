f958289608c908077b494b322b3534efed666cca
Alejandro <alejandro.lago@hotmail.com>
2023-11-18 23:02:17 +0100

    Creada clase MarcoConDibujos

--- graficos/PruebaDibujo.java
+++ graficos/PruebaDibujo.java
@@ -1,11 +1,20 @@
 package graficos;
 
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 public class PruebaDibujo {
     
     public static void main(String[] args) {
         
-        
+
 
     }
+
+}
+
+class MarcoConDibujos extends JFrame {
+
+    
 
 }
