b66a59ca0f88e56b284e2dee51897a33c435c43d
Alejandro <alejandro.lago@hotmail.com>
2023-11-20 21:40:03 +0100

    Dibujar frame

--- graficos/PruebaColores.java
+++ graficos/PruebaColores.java
@@ -33,6 +33,16 @@ class LaminaConColor extends JPanel {
 
         Graphics2D g2 = (Graphics2D) g;
 
+        Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 150);
+
+        g2.draw(rectangulo);
+
+        Ellipse2D elipse = new Ellipse2D.Double();
+
+        elipse.setFrame(rectangulo);
+
+        g2.draw(elipse);
+
     }
 
 }
