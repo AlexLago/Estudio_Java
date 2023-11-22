7908fa51833fd3e71a4f3aa4c74793e2ebd27826
Alejandro <alejandro.lago@hotmail.com>
2023-11-22 21:31:53 +0100

    Añadido JFrame

--- graficos/TrabajarConFuentes.java
+++ graficos/TrabajarConFuentes.java
@@ -7,8 +7,19 @@ public class TrabajarConFuentes {
     
     public static void main(String[] args) {
         
+        
 
+    }
 
+}
+
+class MarcoConFuentes extends JFrame {
+
+    public MarcoConFuentes() {
+
+        setTitle("Prueba con Fuentes");
+        setSize(400,400);
+        
     }
 
 }
