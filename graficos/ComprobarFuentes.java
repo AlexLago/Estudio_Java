186b62a898f97a90e494885f88c88efcbea090d2
Alejandro <alejandro.lago@hotmail.com>
2023-11-22 21:20:27 +0100

    Programa simple verificar fuente

--- graficos/ComprobarFuentes.java
+++ graficos/ComprobarFuentes.java
@@ -5,4 +5,25 @@ import javax.swing.*;
 
 public class ComprobarFuentes {
     
+    public static void main(String[] args) {
+        
+        String fuente = JOptionPane.showInputDialog("Introduce fuente");
+        String [] nombres_fuentes = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        boolean presencia_fuente = false;
+
+        for (String nombre : nombres_fuentes) {
+            
+            if (nombres_fuentes.equals(fuente)) {
+                presencia_fuente = true;
+            }
+        }
+
+        if (presencia_fuente) {
+            System.out.println("Fuente instalada");
+        } else {
+            System.out.println("No está instalada la fuente");
+        }
+
+    }
+
 }
