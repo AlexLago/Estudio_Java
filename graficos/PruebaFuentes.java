aa509f836afa7c73ec6e49dddff3b232f867e29b
Alejandro <alejandro.lago@hotmail.com>
2023-11-22 21:08:56 +0100

    Añadido imports

--- graficos/PruebaFuentes.java
+++ graficos/PruebaFuentes.java
@@ -1,9 +1,13 @@
 package graficos;
 
+import java.awt.GraphicsEnvironment;
+
 public class PruebaFuentes {
     
     public static void main(String[] args) {
         
+        String [] nombres_de_fuentes = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+
     }
-    
+
 }
