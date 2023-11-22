52c037c0e4b10d1b174b2c8bc0e87ab973d1fd41
Alejandro <alejandro.lago@hotmail.com>
2023-11-22 21:10:05 +0100

    Creado foreach

--- graficos/PruebaFuentes.java
+++ graficos/PruebaFuentes.java
@@ -8,6 +8,12 @@ public class PruebaFuentes {
         
         String [] nombres_de_fuentes = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
 
+        for (String nombre_fuente : nombres_de_fuentes) {
+            
+            System.out.println(nombre_fuente);
+
+        }
+
     }
 
 }
