5a6cc2a2e814dcf17671abed6730b7208756967b
Alejandro <alejandro.lago@hotmail.com>
2023-11-24 23:59:32 +0100

    Añadido paintComponent

--- graficos/PruebaImagen2.java
+++ graficos/PruebaImagen2.java
@@ -1,5 +1,6 @@
 package graficos;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
@@ -20,6 +21,29 @@ class Frame extends JFrame {
 
         setTitle("Prueba Imagen");
         setBounds(200, 300, 300, 300);
+
+    }
+
+}
+
+class Lamina extends JTable {
+
+    private Image imagen;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        File direccion = new File("Estudio_Java\\src\\graficos\\material\\imagen.gif");
+
+        try {
+            imagen = ImageIO.read(direccion);
+        } catch (IOException e) {
+            System.out.println("No se ha encontrado la imagen");
+        }
+
+        g.drawImage(imagen, 0, 0, null);
 
     }
 
