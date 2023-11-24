20efed4164af54f992b91ec0166a9eeb72475ecb
Alejandro <alejandro.lago@hotmail.com>
2023-11-24 23:51:06 +0100

    Añadido importes

--- graficos/PruebaImagen2.java
+++ graficos/PruebaImagen2.java
@@ -1,10 +1,14 @@
 package graficos;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
 public class PruebaImagen2 {
     
 public static void main(String[] args) {
     
-    
+
 
 }
 
