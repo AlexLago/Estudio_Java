ade448c2550ced6ad09c36f3dafca3d732c5e885
Alejandro <alejandro.lago@hotmail.com>
2023-11-24 23:53:22 +0100

    Añadido JFrame

--- graficos/PruebaImagen2.java
+++ graficos/PruebaImagen2.java
@@ -6,10 +6,21 @@ import java.io.*;
 
 public class PruebaImagen2 {
     
-public static void main(String[] args) {
-    
+    public static void main(String[] args) {
+        
 
+
+    }
 
 }
+
+class Frame extends JFrame {
+
+    public Frame() {
+
+        setTitle("Prueba Imagen");
+        setBounds(200, 300, 300, 300);
+
+    }
 
 }
