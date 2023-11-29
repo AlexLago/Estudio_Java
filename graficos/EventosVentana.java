735bd2588211f5aee20a3cb19797ba586c273a65
Alejandro <alejandro.lago@hotmail.com>
2023-11-29 12:05:23 +0100

    Añadido imports

--- graficos/EventosVentana.java
+++ graficos/EventosVentana.java
@@ -1,10 +1,13 @@
 package graficos;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class EventosVentana {
     
     public static void main(String[] args) {
         
-        
+
 
     }
 
