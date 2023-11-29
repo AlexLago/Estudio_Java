85b14839e15f9c9b3d7206a15aa825402eefbd10
Alejandro <alejandro.lago@hotmail.com>
2023-11-29 12:42:26 +0100

    Arreglado para ignorar advertencia unused

--- graficos/CreandoMarcos.java
+++ graficos/CreandoMarcos.java
@@ -8,13 +8,20 @@ public class CreandoMarcos {    // Prueba simple con JFrames
     
     public static void main(String[] args) {
 
-        @SuppressWarnings("unused")
-        miMarco marco = new miMarco();
+        /*@SuppressWarnings("unused")
+        miMarco marco = new miMarco();*/
+
+        JFrame miMarco = new JFrame();
+
+        miMarco.setBounds(500, 300, 250, 250);
+        miMarco.setExtendedState(Frame.MAXIMIZED_BOTH);
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        miMarco.setVisible(true);
 
     }
 }
 
-class miMarco extends JFrame{
+/*class miMarco extends JFrame{
 
     public miMarco() {
 
@@ -27,4 +34,4 @@ class miMarco extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
-}
+}*/
