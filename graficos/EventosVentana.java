a505c77881b9ed8685d887161cf5a97514deed2f
Alejandro <alejandro.lago@hotmail.com>
2023-11-29 12:38:14 +0100

    Limpiado codigo

--- graficos/EventosVentana.java
+++ graficos/EventosVentana.java
@@ -19,16 +19,7 @@ public class EventosVentana {
 
 class MarcoVentana extends JFrame{
 
-    /*private String titulo;
-    private int x, y, width, height;*/
-
     public MarcoVentana(String titulo, int x, int y, int width, int height) {
-
-        /*this.titulo = titulo;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;*/
 
         setTitle(titulo);
         setBounds(x, y, width, height);
