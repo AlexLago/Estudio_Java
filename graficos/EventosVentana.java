84a97dbbec8b9ee2b9050f8ae4fd45a3e7cce907
Alejandro <alejandro.lago@hotmail.com>
2023-11-29 12:12:26 +0100

    Añadido métodos de WindowListener

--- graficos/EventosVentana.java
+++ graficos/EventosVentana.java
@@ -29,6 +29,18 @@ class MarcoVentana extends JFrame{
 
 class M_Ventana implements WindowListener{
 
-    
+    public void windowActivated(WindowEvent e){}
+
+    public void windowClosed(WindowEvent e){}
+
+    public void windowClosing(WindowEvent e){}
+
+    public void windowDeactivated(WindowEvent e){}
+
+    public void windowDeiconified(WindowEvent e){}
+
+    public void windowIconified(WindowEvent e){}
+
+    public void windowOpened(WindowEvent e){}
 
 }
