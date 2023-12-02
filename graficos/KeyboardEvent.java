6dcbc28e2950f5d96a4fef75427bdade4499d6cb
Alejandro <alejandro.lago@hotmail.com>
2023-12-02 23:17:03 +0100

    Added class FrameWithKeys

--- graficos/KeyboardEvent.java
+++ graficos/KeyboardEvent.java
@@ -1,12 +1,20 @@
 package graficos;
 
+import javax.swing.JFrame;
+
 public class KeyboardEvent {
     
 
     public static void main(String[] args) {
         
-        
+
 
     }
+
+}
+
+class FrameWithKeys extends JFrame {
+
+    
 
 }
