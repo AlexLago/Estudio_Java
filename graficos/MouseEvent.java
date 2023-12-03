29203345381139adc94da039d482189d7b002f51
Alejandro <alejandro.lago@hotmail.com>
2023-12-03 20:36:00 +0100

    Renamed class

--- graficos/MouseEvent.java
+++ graficos/MouseEvent.java
@@ -1,6 +1,7 @@
 package graficos;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 public class MouseEvent {
     
@@ -25,7 +26,7 @@ class MouseFrame extends JFrame {
 
 }
 
-class MouseListener implements MouseListener {
+class Mouse implements MouseListener {
 
 
     
