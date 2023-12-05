25b9aca1c2cd2310916e4ff416bf946aacc2a526
Alejandro <alejandro.lago@hotmail.com>
2023-12-05 13:44:57 +0100

    Added main method

--- graficos/FocusWindow.java
+++ graficos/FocusWindow.java
@@ -1,11 +1,15 @@
 package graficos;
 
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowFocusListener;
-
+import java.awt.event.*;
 import javax.swing.JFrame;
 
 public class FocusWindow extends JFrame implements WindowFocusListener{
+
+    public static void main(String[] args) {
+        
+
+        
+    }
 
     @Override
     public void windowGainedFocus(WindowEvent e) {}
