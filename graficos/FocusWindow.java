263ee8f4f06c6e144775109d0c5b0bdd6ee2e24c
Alejandro <alejandro.lago@hotmail.com>
2023-12-05 13:44:18 +0100

    Added interface methods

--- graficos/FocusWindow.java
+++ graficos/FocusWindow.java
@@ -1,7 +1,16 @@
 package graficos;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
 import javax.swing.JFrame;
 
-public class FocusWindow extends JFrame {
+public class FocusWindow extends JFrame implements WindowFocusListener{
+
+    @Override
+    public void windowGainedFocus(WindowEvent e) {}
+
+    @Override
+    public void windowLostFocus(WindowEvent e) {}
     
 }
