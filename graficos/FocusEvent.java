3b060d3a86c52a5479058e93268c9d6e5391bb1a
Alejandro <alejandro.lago@hotmail.com>
2023-12-05 12:51:29 +0100

    Added FocusFrame class

--- graficos/FocusEvent.java
+++ graficos/FocusEvent.java
@@ -1,11 +1,19 @@
 package graficos;
 
+import javax.swing.JFrame;
+
 public class FocusEvent {
     
     public static void main(String[] args) {
         
-        
+
 
     }
+
+}
+
+class FocusFrame extends JFrame {
+
+    
 
 }
