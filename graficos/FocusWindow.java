7170ce681e1d397e81e2d1fb3a9b351710e396fe
Alejandro <alejandro.lago@hotmail.com>
2023-12-05 13:49:01 +0100

    Added 2 instances FocusWindow

--- graficos/FocusWindow.java
+++ graficos/FocusWindow.java
@@ -5,9 +5,25 @@ import javax.swing.JFrame;
 
 public class FocusWindow extends JFrame implements WindowFocusListener{
 
+    FocusWindow frame1;
+    FocusWindow frame2;
+
     public static void main(String[] args) {
         
 
+
+    }
+
+    public void start() {
+
+        frame1 = new FocusWindow();
+        frame2 = new FocusWindow();
+
+        frame1.setVisible(true);
+        frame2.setVisible(true);
+
+        frame1.setBounds(300, 100, 600, 350);
+        frame1.setBounds(1200, 100, 600, 350);
         
     }
 
