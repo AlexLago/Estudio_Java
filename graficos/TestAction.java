538f151574e10ef42e6f281bfbec32418616c936
Alejandro <alejandro.lago@hotmail.com>
2023-12-05 14:14:47 +0100

    Added ActionFrame class

--- graficos/TestAction.java
+++ graficos/TestAction.java
@@ -1,11 +1,19 @@
 package graficos;
 
+import javax.swing.JFrame;
+
 public class TestAction {
     
     public static void main(String[] args) {
         
-        
+
 
     }
+
+}
+
+class ActionFrame extends JFrame {
+
+    
 
 }
