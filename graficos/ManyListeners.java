ed18b960c4eb60ba1f3ee952d8098a83efd565fa
Alejandro <alejandro.lago@hotmail.com>
2023-12-06 17:17:18 +0100

    Added class MainFrame

--- graficos/ManyListeners.java
+++ graficos/ManyListeners.java
@@ -1,12 +1,20 @@
 package graficos;
 
+import javax.swing.JFrame;
+
 public class ManyListeners {
     
 
     public static void main(String[] args) {
         
-        
+
 
     }
+
+}
+
+class MainFrame extends JFrame {
+
+    
 
 }
