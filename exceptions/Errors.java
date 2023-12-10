1b79471d6adb0bba9527782373ac29ac20774de7
Alejandro <alejandro.lago@hotmail.com>
2023-12-10 21:06:02 +0100

    Added array my_array

--- exceptions/Errors.java
+++ exceptions/Errors.java
@@ -4,7 +4,13 @@ public class Errors {
     
     public static void main(String[] args) {
         
-        
+        int[] my_array = new int[5];
+
+        my_array[0] = 5;
+        my_array[1] = 38;
+        my_array[2] = 15;
+        my_array[3] = 92;
+        my_array[4] = 71;
 
     }
 
