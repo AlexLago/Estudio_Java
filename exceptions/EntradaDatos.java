820faf28a243c6adef28f39c52f6bb13fe8753a0
Alejandro <alejandro.lago@hotmail.com>
2023-12-12 12:43:42 +0100

    Added messages

--- exceptions/EntradaDatos.java
+++ exceptions/EntradaDatos.java
@@ -4,7 +4,9 @@ public class EntradaDatos {
     
     public static void main(String[] args) {
         
-        
+        System.out.println("¿Que deseas hacer?");
+        System.out.println("1. Introducir datos");
+        System.out.println("2. Salir del programa");
 
     }
 
