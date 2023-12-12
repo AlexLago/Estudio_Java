251b7cd2bebfbc42cd68be4f4f756b1e31da726c
Alejandro <alejandro.lago@hotmail.com>
2023-12-12 13:15:56 +0100

    Added InputDialog

--- exceptions/CompruebaMail.java
+++ exceptions/CompruebaMail.java
@@ -1,9 +1,13 @@
 package exceptions;
 
+import javax.swing.JOptionPane;
+
 public class CompruebaMail {
     
     public static void main(String[] args) {
         
+        String mail = JOptionPane.showInputDialog("Introduce email");
+
         
 
     }
