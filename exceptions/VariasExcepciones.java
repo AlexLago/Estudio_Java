4eba27fe0213948d884f8bc7a83b58e736d6bac6
Alejandro <alejandro.lago@hotmail.com>
2023-12-12 14:04:52 +0100

    Added division method

--- exceptions/VariasExcepciones.java
+++ exceptions/VariasExcepciones.java
@@ -1,10 +1,21 @@
 package exceptions;
 
+import javax.swing.JOptionPane;
+
 public class VariasExcepciones {
     
     public static void main(String[] args) {
         
-        
+        division();
+
+    }
+
+    static void division() {
+
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el dividendo"));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el divisor"));
+
+        System.out.println("El resultado es: " + num1/num2);
 
     }
 
