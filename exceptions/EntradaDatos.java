e353c9dc111e577485841bfe5d69205775f8b5e3
Alejandro <alejandro.lago@hotmail.com>
2023-12-12 12:51:28 +0100

    Added pedirDatos method

--- exceptions/EntradaDatos.java
+++ exceptions/EntradaDatos.java
@@ -27,4 +27,24 @@ public class EntradaDatos {
 
     }
 
+    static void pedirDatos() {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce tu nombre, por favor");
+
+        String nombre_usuario = entrada.nextLine();
+
+        System.out.println("Introduce edad, por favor");
+
+        int edad = entrada.nextInt();
+
+        System.out.println("Hola " + nombre_usuario + " . El año que viene tendras " + (edad+1) + " años");
+
+        entrada.close();
+
+        System.out.println("Hemos terminado");
+
+    }
+
 }
