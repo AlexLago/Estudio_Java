5121a7a380ff4463b0300ce56fbdb73d471bb1d4
Alejandro <alejandro.lago@hotmail.com>
2023-12-14 21:49:26 +0100

    Added ints

--- exceptions/Aleatorios.java
+++ exceptions/Aleatorios.java
@@ -1,12 +1,16 @@
 package exceptions;
 
+import javax.swing.JOptionPane;
+
 public class Aleatorios {
     
     public static void main(String[] args) {
         
-
-
+        int elementos = Integer.parseInt(JOptionPane.showInputDialog("Introduce elementos de la matriz"));
+        int random_num[] = new int[elementos];
         
+        
+
     }
 
 }
