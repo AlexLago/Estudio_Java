342efc1863b23a0a8b21dfe071f712ff1b6a2d8c
Alejandro <alejandro.lago@hotmail.com>
2023-12-21 13:21:13 +0100

    Added FileWriter and Escribiendo instance

--- acceso_ficheros/AccesoFicheros.java
+++ acceso_ficheros/AccesoFicheros.java
@@ -2,6 +2,7 @@ package acceso_ficheros;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class AccesoFicheros {
@@ -11,6 +12,10 @@ public class AccesoFicheros {
         LeerFichero accediendo = new LeerFichero();
 
         accediendo.lee();
+
+        Escribiendo escritura = new Escribiendo();
+
+        escritura.escribir();
 
     }
 
@@ -41,6 +46,30 @@ class LeerFichero {
             System.out.println("No se ha encontrado el archivo.");
         } catch(IOException e) {
             System.out.println("Fallo al leer el archivo.");
+        }
+
+    }
+
+}
+
+class Escribiendo {
+
+    public void escribir() {
+
+        String frase = "Esto es una prueba de escritura.";
+
+        try {
+
+            FileWriter escritura = new FileWriter("acceso_ficheros\\nuevo_texto.txt");
+
+            for(int i=0; i<frase.length(); i++) {
+                escritura.write(frase.charAt(i));
+            }
+
+            escritura.close();
+
+        } catch (IOException e) {
+            System.out.println("No se ha encontrado la carpeta.");
         }
 
     }
