4881701405e0981a57eafedb4c9d1a6b92689312
Alejandro <alejandro.lago@hotmail.com>
2023-12-21 11:40:56 +0100

    Added reader and LeerFichero instance

--- acceso_ficheros/AccesoFicheros.java
+++ acceso_ficheros/AccesoFicheros.java
@@ -1,12 +1,16 @@
 package acceso_ficheros;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class AccesoFicheros {
     
     public static void main(String[] args) {
         
+        LeerFichero accediendo = new LeerFichero();
 
+        accediendo.lee();
 
     }
 
@@ -16,7 +20,24 @@ class LeerFichero {
 
     public void lee() {
 
-        FileReader entrada = new FileReader("");
+        try {
+
+            FileReader entrada = new FileReader("src/acceso_ficheros/ejemplo.txt");
+            int c = entrada.read();
+
+            while (c != -1) {
+                
+                c = entrada .read();
+
+                System.out.println(c);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el archivo.");
+        } catch(IOException e) {
+            System.out.println("Fallo al leer el archivo.");
+        }
 
     }
 
