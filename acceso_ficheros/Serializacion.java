ea61121ce9d612a04df5aa9e0b6b90bac6cfdecf
Alejandro <alejandro.lago@hotmail.com>
2024-01-24 12:59:53 +0100

    Added constructor method Empleado

--- acceso_ficheros/Serializacion.java
+++ acceso_ficheros/Serializacion.java
@@ -11,4 +11,17 @@ public class Serializacion {
 
 class Empleado {
 
+    String nombre;
+    double sueldo;
+    int mes, ano, dia;
+
+    public Empleado(String nombre, double sueldo, int ano, int mes, int dia) {
+
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+
+        GregorianCalendar calendario = new GregorianCalendar(ano, mes-1, dia);
+
+    }
+
 }
