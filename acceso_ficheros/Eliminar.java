09f5b13f073b029a73626888ef5d6278e13a140e
Alejandro <alejandro.lago@hotmail.com>
2024-01-24 16:45:55 +0100

    Added delete

--- acceso_ficheros/Eliminar.java
+++ acceso_ficheros/Eliminar.java
@@ -1,10 +1,14 @@
 package acceso_ficheros;
 
+import java.io.File;
+
 public class Eliminar {
     
     public static void main(String[] args) {
         
-        
+        File ruta = new File("acceso_ficheros" + File.separator + "prueba_texto.txt");
+
+        ruta.delete();
 
     }
 
