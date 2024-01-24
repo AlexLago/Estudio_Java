8b3bd0359f97388fef864d373ce4e3962e28ad69
Alejandro <alejandro.lago@hotmail.com>
2024-01-24 15:15:06 +0100

    Added file object

--- acceso_ficheros/PruebaRutas.java
+++ acceso_ficheros/PruebaRutas.java
@@ -1,10 +1,12 @@
 package acceso_ficheros;
 
+import java.io.*;
+
 public class PruebaRutas {
     
     public static void main(String[] args) {
 
-        
+        File archivo = new File("ejemplo_archivo");
 
     }
 
