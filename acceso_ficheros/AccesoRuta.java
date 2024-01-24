8f7ca9f39d2472c44613ee830f37928b904160df
Alejandro <alejandro.lago@hotmail.com>
2024-01-24 15:24:45 +0100

    Added foreach

--- acceso_ficheros/AccesoRuta.java
+++ acceso_ficheros/AccesoRuta.java
@@ -6,7 +6,13 @@ public class AccesoRuta {
     
     public static void main(String[] args) {
 
-        
+        File ruta = new File("acceso_ficheros");
+
+        String[] nombre_archivos = ruta.list();
+
+        for (String string : nombre_archivos) {
+            System.out.println(string);
+        }
 
     }
 
