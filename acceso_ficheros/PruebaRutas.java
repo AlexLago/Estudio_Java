e306e1e6d4bea0e582a911d56e2688d7a3cc626b
Alejandro <alejandro.lago@hotmail.com>
2024-01-24 15:16:07 +0100

    Added print

--- acceso_ficheros/PruebaRutas.java
+++ acceso_ficheros/PruebaRutas.java
@@ -7,6 +7,8 @@ public class PruebaRutas {
     public static void main(String[] args) {
 
         File archivo = new File("ejemplo_archivo");
+        
+        System.out.println(archivo.getAbsolutePath());
 
     }
 
